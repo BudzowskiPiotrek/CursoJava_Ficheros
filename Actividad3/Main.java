3c3dc3f722b6d0ade016e7bf034bd7e32a52876b
Piotrek Budzowski <piobudzows@gmail.com>
2025-04-22 11:14:54 +0200

    Create Main.java

---
+++ Actividad3/Main.java
@@ -0,0 +1,43 @@
+package CursoJava_Ficheros.Actividad3;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        String userHome = System.getProperty("user.home");
+        String rutaEscritorio = userHome + "/Desktop/";
+
+        System.out.println("Dime el nombre de la carpeta donde esta el archivo: ");
+        String nombreCarpeta = sn.nextLine();
+        System.out.println("Dime el nombre de archivo: ");
+        String nombreFichero = sn.nextLine();
+
+        File archivo = new File(rutaEscritorio + nombreCarpeta + "/" + nombreFichero + ".txt");
+        boolean eliminado = archivo.delete();
+
+        if (eliminado) {
+            System.out.println("Exito");
+        } else {
+            System.out.println("no exito");
+        }
+
+        System.out.println("Quieres borrar tambien su carpeta\nSi");
+        // HAHAHAHAH
+        String respuesta = "Si";
+        if (respuesta.equals("Si")) {
+            File carpeta = new File(rutaEscritorio + nombreCarpeta);
+            boolean eliminado2 = carpeta.delete();
+
+            if (eliminado2) {
+                System.out.println("Exito");
+            } else {
+                System.out.println("no exito");
+            }
+        }
+        sn.close();
+    }
+
+}
