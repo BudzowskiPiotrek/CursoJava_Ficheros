c314af4b0ffa7954f593b205adfd0e5c8d234372
Piotrek Budzowski <piobudzows@gmail.com>
2025-05-06 11:08:52 +0200

    Create Main.java

---
+++ Actividad8/Main.java
@@ -0,0 +1,25 @@
+package CursoJava_Ficheros.Actividad8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("CursoJava_Ficheros/Actividad8/Estudiante.java"))) {
+            String linea;
+            StringBuilder documentoNuevo = new StringBuilder();
+            while ((linea = bufferedReader.readLine()) != null) {
+                if (!linea.contains("//")) {
+                    documentoNuevo.append(linea + "\n");
+                } else {
+                    linea = linea.substring(0, linea.indexOf("//"));
+                }
+            }
+            System.out.println(documentoNuevo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
